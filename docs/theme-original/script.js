a1cb9b102f721cb711a298f540c867945e16985f
Your Name <you@example.com>
2019-04-25 11:30:20 +0800

    fix style

--- docs/theme-original/script.js
+++ docs/theme-original/script.js
@@ -108,6 +108,28 @@ jQuery(document).ready(function() {
             $('.progress').stop(true, false, true).fadeToggle(100);
         });
     }
+
+    // allow keyboard control for prev/next links
+    jQuery(function() {
+        jQuery('.nav-prev').click(function(){
+            location.href = jQuery(this).attr('href');
+        });
+        jQuery('.nav-next').click(function() {
+            location.href = jQuery(this).attr('href');
+        });
+    });
+    jQuery(document).keydown(function(e) {
+      // prev links - left arrow key
+      if(e.which == '37') {
+        jQuery('.nav.nav-prev').click();
+      }
+      // next links - right arrow key
+      if(e.which == '39') {
+        jQuery('.nav.nav-next').click();
+      }
+    });
+
+
 });
 
 jQuery(window).on('load', function() {
